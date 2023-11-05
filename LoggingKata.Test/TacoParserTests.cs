de676dc3481415aaa24ce296081fbb1db5044f25
Finch0311 <132722383+Finch0311@users.noreply.github.com>
2023-11-05 17:44:50 -0600

    tacoParser is working

--- LoggingKata.Test/TacoParserTests.cs
+++ LoggingKata.Test/TacoParserTests.cs
@@ -43,6 +43,16 @@ namespace LoggingKata.Test
 
 
         //TODO: Create a test ShouldParseLatitude
+        [Theory]
+        [InlineData("34.073638, -84.677017, Taco Bell Acwort...", 34.073638)]
+        public void ShouldParseLatitude(string line, double expected)
+        {
+            var tacoParserInstance = new TacoParser();
+
+            var actual = tacoParserInstance.Parse(line);
+
+            Assert.Equal(expected, actual.Location.Latitude);
+        }
 
     }
 }
--- LoggingKata/TacoParser.cs
+++ LoggingKata/TacoParser.cs
@@ -17,25 +17,25 @@
             // If your array.Length is less than 3, something went wrong
             if (cells.Length < 3)
             {
-                // Log that and return null
+                // done Log that and return null
                 logger.LogWarning("Less than three items. Incomplete data.");
                 // Do not fail if one record parsing fails, return null
                 return null; // TODO Implement
             }
 
-            // grab the latitude from your array at index 0
+            //done grab the latitude from your array at index 0
             var latiTude = double.Parse(cells[0]);
-            // grab the longitude from your array at index 1
+            //done grab the longitude from your array at index 1
             var lonGitude = double.Parse(cells[1]);
-            // grab the name from your array at index 2
+            //done grab the name from your array at index 2
             var locationName = (cells[2]);
-            // Your going to need to parse your string as a `double`
+            //done Your going to need to parse your string as a `double`
             // which is similar to parsing a string as an `int`
 
-            // You'll need to create a TacoBell class
+            // doneYou'll need to create a TacoBell class
             // that conforms to ITrackable
 
-            // Then, you'll need an instance of the TacoBell class
+            // doneThen, you'll need an instance of the TacoBell class
             // With the name and point set correctly
 
             var point = new Point();
@@ -45,7 +45,7 @@
             var tacoBell = new TacoBell();
             tacoBell.Name = locationName;
             tacoBell.Location = point;
-            // Then, return the instance of your TacoBell class
+            //done Then, return the instance of your TacoBell class
             // Since it conforms to ITrackable
 
             return tacoBell;
